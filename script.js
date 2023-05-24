cd8bdab5046fab675a13acbd8bb584c69b7301f8
Evan Fauzi <85676268+pranendraa@users.noreply.github.com>
2023-05-24 14:45:23 +0700

    tes

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const dp = new DPlayer({
     video: {
         url: videoUrl,
         type: 'auto',
-        thumbnails: 'img/bg.jpg'
+        thumbnails: '/img/bg.jpg'
     },
     subtitle: subtitleUrl ? {
         url: subtitleUrl,
