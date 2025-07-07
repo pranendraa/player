bbc52eda28ec523bb4a28e8b8025f236b7d7b522
Evan Fauzi <85676268+pranendraa@users.noreply.github.com>
2025-07-07 16:08:42 +0700

    hapus tdk guna

---
+++ play/script.js
@@ -0,0 +1,63 @@
+const urlParams = new URLSearchParams(window.location.search);
+const videoUrl = urlParams.get('url');
+const subtitleUrl = urlParams.get('subtitle'); // Mendapatkan URL subtitle dari parameter URL
+document.title = "Web Player with DPlayer - " + videoUrl;
+const dp = new DPlayer({
+    container: document.getElementById('dplayer'),
+    screenshot: true,
+    autoplay: true,
+    hotkey: true,
+    preventClickToggle: true,
+    video: {
+        url: videoUrl,
+        type: 'auto'
+    },
+    subtitle: subtitleUrl ? {
+        url: subtitleUrl,
+        type: 'webvtt',
+        fontSize: '35px',
+        color: '#ffff00'
+    } : null,
+    hls: true,
+    dash: true,
+    xhrSetup: {
+        headers: {
+            'Referer': 'https://dplayer.js.org/'
+        }
+    },
+    pluginOptions: {
+        hls: {
+            enable: true
+        },
+        dash: {
+            enable: true
+        },
+        flv: {
+            enable: true
+        }
+    }
+});
+
+// Deteksi saat video memasuki fullscreen
+dp.on('fullscreen', function () {
+// Tambahkan class CSS untuk tampilan fullscreen video
+    dp.container.classList.add('dplayer-fullscreen');
+});
+
+// Deteksi saat video keluar dari fullscreen
+dp.on('fullscreen_cancel', function () {
+// Hapus class CSS untuk tampilan fullscreen video
+    dp.container.classList.remove('dplayer-fullscreen');
+});
+
+// Deteksi saat video memasuki mode web-fullscreen
+dp.on('webfullscreen', function () {
+// Tambahkan kelas CSS untuk menampilkan video player di tengah
+    dp.container.classList.add('dplayer-web-fullscreen');
+});
+
+// Deteksi saat video keluar dari mode web-fullscreen
+dp.on('webfullscreen_cancel', function () {
+// Hapus kelas CSS untuk menampilkan video player di tengah
+    dp.container.classList.remove('dplayer-web-fullscreen');
+});
